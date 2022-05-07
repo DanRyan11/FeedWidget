04e0b158a8426eb40b71133b28ec0d26bd93dcbd
Danilo Ryan Oliveira <47749142+DanRyan11@users.noreply.github.com>
2022-05-07 19:48:38 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen( process.env.PORT || 3333, () => {
-    console.log('Server started on port 3333');
+const serverPort = process.env.PORT || 3333
+
+app.listen( serverPort, () => {
+    console.log('Server started on port '+serverPort);
 })
